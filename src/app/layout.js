9fc28646329e5377039dfc68fddbf6404d5d3238
Md Shad Khan <devshaad@hustle.local>
2024-05-22 14:16:40 +0530

    feat: pwa added

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,12 +6,13 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Daily Chit Chat | Dev Shad",
   description: "Daily Chat Personal Group Web",
+  manifest: '/manifest.webmanifest'
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body suppressHydrationWarning={true} className={inter.className}>{children}</body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
